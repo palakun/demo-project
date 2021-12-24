758ad12febda40971912b4fd38d07e5d2651a796
Ibrahim Ersu <imeu2005@gmail.com>
2021-12-24 12:43:16 -0500

    Shawarma

--- src/test/java/cbt/CukesRunner.java
+++ src/test/java/cbt/CukesRunner.java
@@ -7,10 +7,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "json:target/cucumber.json",
-                "html:target/cucumber/",
-                "junit:target/junit/junit-report.xml",
-                "rerun:target/rerun.txt"},
+//                "json:target/cucumber.json",
+//                "html:target/cucumber/",
+//                "junit:target/junit/junit-report.xml",
+//                "rerun:target/rerun.txt"
+        },
         features = "src/test/resources/cbt"
 )
 public class CukesRunner {}
